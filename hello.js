e8c513abbdef3e6dea02840943876f10dffa69c0
NekoCarreraDesigns <denvermaas@gmail.com>
2022-07-20 14:26:51 -0600

    changed setting in tsconfig.json, and added start command to package.json

--- hello.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function padLeft(padding, input) {
-    if (typeof padding === "number") {
-        return new Array(padding + 1).join(" ") + input;
-    }
-    return padding + input;
-}
